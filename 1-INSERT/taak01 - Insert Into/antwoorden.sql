edaef4f4dae476832b8d7bec9c1fae628884b73f
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-06-11 10:42:22 +0200

    gedaan

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO studenten (voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES ('henk', 'kattenbaks', '9A', 'jol 10', '1188WE', '2001-03-24', 'Amsterdam');
